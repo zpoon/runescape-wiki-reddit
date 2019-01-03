bdc7db50150e2100623600549e3e370aeded0cc6
Nicholas Torkos <n.torkos@gmail.com>
2019-01-03 03:11:15 -0500

    added description text

--- runescape-wiki.py
+++ runescape-wiki.py
@@ -31,14 +31,26 @@ def process_comment(comment):
             response = requests.get("https://oldschool.runescape.wiki/api.php?action=opensearch&search=" + match.group(1))
             data = response.json()
             if data[1]:
-                reply_text = "I have found a OSRS Wiki article for your search. \n\n **[" + data[1][0] + "](" + data[3][0] + ")** | " + data[3][0] + " \n\n --- \n\n **^^^RuneScape ^^^Wiki ^^^linker** ^^^| ^^^This ^^^was ^^^generated ^^^automatically. ^^^| ^^^View ^^^me ^^^on ^^^[GitHub](https://github.com/zpoon/runescape-wiki-reddit)."
-                comment.reply(reply_text)
+                parse_page = requests.get("https://oldschool.runescape.wiki/api.php?action=parse&redirects=1&format=json&page=" + match.group(1))
+                page_json = parse_page.json()
+                try:
+                    description = page_json['parse']['properties'][1]['*']
+                    reply_text = "I found 1 OSRS Wiki article for your search. \n\n **[" + data[1][0] + "](" + data[3][0] + ")** | " + data[3][0] + " \n\n >" + description + " \n\n --- \n\n **^^^RuneScape ^^^Wiki ^^^linker** ^^^| ^^^This ^^^was ^^^generated ^^^automatically. ^^^| ^^^View ^^^me ^^^on ^^^[GitHub](https://github.com/zpoon/runescape-wiki-reddit)."
+                    comment.reply(reply_text)
+                except KeyError:
+                    print "No page found: " + match.group(1)
         if comment.subreddit == "runescape":
             response = requests.get("https://runescape.wiki/api.php?action=opensearch&search=" + match.group(1))
             data = response.json()
             if data[1]:
-                reply_text = "I have found a RuneScape Wiki article for your search. \n\n **[" + data[1][0] + "](" + data[3][0] + ")** | " + data[3][0] + " \n\n --- \n\n **^^^RuneScape ^^^Wiki ^^^linker** ^^^| ^^^This ^^^was ^^^generated ^^^automatically. ^^^| ^^^View ^^^me ^^^on ^^^[GitHub](https://github.com/zpoon/runescape-wiki-reddit)."
-                comment.reply(reply_text)
+                parse_page = requests.get("https://runescape.wiki/api.php?action=parse&redirects=1&format=json&page=" + match.group(1))
+                page_json = parse_page.json()
+                try:
+                    description = page_json['parse']['properties'][1]['*']
+                    reply_text = "I found 1 RuneScape Wiki article for your search. \n\n **[" + data[1][0] + "](" + data[3][0] + ")** | " + data[3][0] + " \n\n >" + description + " \n\n --- \n\n **^^^RuneScape ^^^Wiki ^^^linker** ^^^| ^^^This ^^^was ^^^generated ^^^automatically. ^^^| ^^^View ^^^me ^^^on ^^^[GitHub](https://github.com/zpoon/runescape-wiki-reddit)."
+                    comment.reply(reply_text)
+                except KeyError:
+                    print "No page found: " + match.group(1)
 
 
 if __name__ == '__main__':
